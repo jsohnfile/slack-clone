53ff08884ed5db16b3a661218a468c7c1f6ae9ce
jsohnfile <hoodieee@Jaes-MacBook-Air.local>
2020-10-03 16:41:49 -0600

    Completed basic header

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,28 @@
 import React from 'react'
 import "./Header.css";
 import { Avatar } from "@material-ui/core";
-import { AccessTimeIcon } from "@material-ui/icons/AccessTime";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import SearchIcon from "@material-ui/icons/Search";
+import HelpOutlineIcon from "@material-ui/icons/HelpOutline";
 
 function Header() {
     return (
         <div className="header">
-           <div className="header__left">
-           <Avatar className="header__avatar" alt="jaesohn"  src="" />
-            <AccessTimeIcon />
+            <div className="header__left">
+                <Avatar className="header__avatar" alt="jaesohn"  src="" />
+                <AccessTimeIcon />
                {/* Avatar for logged in user*/}
                {/* Time icon */}
-           </div> 
-           <div className="header__search">
+            </div> 
+            <div className="header__search">
+                <SearchIcon />
+                <input placeholder="Search..." type="text"></input>
                {/* Search icon */}
                {/* input */}
-           </div>
-           <div className="header__right">
-               {/* help icon */}
-           </div>
+            </div>
+            <div className="header__right">
+               <HelpOutlineIcon />
+            </div>
         </div>
     )
 }
